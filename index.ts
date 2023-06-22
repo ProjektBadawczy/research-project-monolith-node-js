70a1b844f2848709071e1451d9b0d3c06966cf93
Weronika <weronika.piotrowska.pl@gmail.com>
2023-06-22 21:57:40 +0200

    Added cluster for correct scaling

--- index.ts
+++ index.ts
@@ -1,10 +1,26 @@
 import express from 'express';
 import {route} from "./routes";
+import cluster from 'cluster';
+import os from "os";
 
 const app = express();
+const cpuCount = os.cpus().length;
 
-app.use('/', route);
+if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
 
-app.listen(8001, () => {
-    console.log('Server started');
-});
+    app.use('/', route);
+
+    app.listen(8001, () => {
+        console.log('Server started');
+    });
+  
+    // Fork workers.
+    for (let i = 0; i < cpuCount; i++) {
+      cluster.fork();
+    }
+  
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`worker ${worker.process.pid} died`);
+    });
+  } 
