df99f05ec598f32f899e817954b0dda8e638747d
Weronika <weronika.piotrowska.pl@gmail.com>
2023-06-22 22:18:27 +0200

    fixed scaling

--- index.ts
+++ index.ts
@@ -3,24 +3,21 @@ import {route} from "./routes";
 import cluster from 'cluster';
 import os from "os";
 
-const app = express();
+
 const cpuCount = os.cpus().length;
 
 if (cluster.isPrimary) {
-    console.log(`Primary ${process.pid} is running`);
-
-    app.use('/', route);
-
-    app.listen(8001, () => {
-        console.log('Server started');
-    });
-  
     // Fork workers.
     for (let i = 0; i < cpuCount; i++) {
       cluster.fork();
     }
-  
-    cluster.on('exit', (worker, code, signal) => {
-      console.log(`worker ${worker.process.pid} died`);
+  } else {
+    // Workers can share any TCP connection
+    // In this case it is an HTTP server
+    const app = express();
+    app.use('/', route);
+    app.listen(8001, () => {
+        console.log('Server started');
     });
-  } 
+    console.log(`Worker ${process.pid} started`);
+  }
